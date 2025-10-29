ee002c623568aa0e06cabd2811a8c28d7ec384d2
camilasilvadelgado <camilasd01@gmail.com>
2025-10-29 15:05:50 +0100

    Preprocessing file edits

--- mialab/filtering/preprocessing.py
+++ mialab/filtering/preprocessing.py
@@ -6,6 +6,7 @@ import warnings
 
 import pymia.filtering.filter as pymia_fltr
 import SimpleITK as sitk
+import numpy as np
 
 
 class ImageNormalization(pymia_fltr.Filter):
@@ -29,10 +30,13 @@ class ImageNormalization(pymia_fltr.Filter):
         img_arr = sitk.GetArrayFromImage(image)
 
         # todo: normalize the image using numpy
-        warnings.warn('No normalization implemented. Returning unprocessed image.')
+        img_arr = sitk.GetArrayFromImage(image).astype(np.float32)
+        min_val = np.min(img_arr)
+        max_val = np.max(img_arr)
+        img_normalised = (img_arr - min_val) / (max_val - min_val)
 
-        img_out = sitk.GetImageFromArray(img_arr)
-        img_out.CopyInformation(image)
+        img_out = sitk.GetImageFromArray(img_normalised)
+        img_out.CopyInformation(img_normalised) # copy spacing, origin, direction
 
         return img_out
 
@@ -78,7 +82,11 @@ class SkullStripping(pymia_fltr.Filter):
         mask = params.img_mask  # the brain mask
 
         # todo: remove the skull from the image by using the brain mask
-        warnings.warn('No skull-stripping implemented. Returning unprocessed image.')
+        img_arr = sitk.GetArrayFromImage(image)
+        mask_arr = sitk.GetArrayFromImage(mask)
+        img_arr = img_arr * mask_arr
+        image = sitk.GetImageFromArray(img_arr)
+        image.CopyInformation(image) # copy spacing, origin, direction
 
         return image
 
@@ -128,11 +136,17 @@ class ImageRegistration(pymia_fltr.Filter):
 
         # todo: replace this filter by a registration. Registration can be costly, therefore, we provide you the
         # transformation, which you only need to apply to the image!
-        warnings.warn('No registration implemented. Returning unregistered image')
 
         atlas = params.atlas
         transform = params.transformation
         is_ground_truth = params.is_ground_truth  # the ground truth will be handled slightly different
+        if is_ground_truth:
+            image = sitk.Resample(image, atlas, transform, sitk.sitkNearestNeighbor, 0, image.GetPixelID())
+        else:
+            image = sitk.Resample(image, atlas, transform, sitk.sitkLinear, 0.0, image.GetPixelID())
+
+        
+        
 
         # note: if you are interested in registration, and want to test it, have a look at
         # pymia.filtering.registration.MultiModalRegistration. Think about the type of registration, i.e.
